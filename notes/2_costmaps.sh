302374dc8abf2bbdc61a1c4da6b16cb626247fba
sierra-95 <michaelmachohi@gmail.com>
2025-10-06 00:22:02 +0300

    path planning and navigation

---
+++ notes/2_costmaps.sh
@@ -0,0 +1,4 @@
+ros2 run nav2_costmap_2d nav2_costmap_2d --ros-args --params-file /home/sierra-95/Documents/ros2_path_planning_and_navigation/config/costmap.yaml
+ros2 lifecycle nodes
+ros2 lifecycle set /costmap/costmap 1
+ros2 lifecycle set /costmap/costmap 3
