21e0cfced9d37f30eb79741e36478d2b43279707
sierra-95 <michaelmachohi@gmail.com>
2025-10-05 22:57:21 +0300

    path planning and navigation

---
+++ notes/1_path_planning.sh
@@ -0,0 +1,5 @@
+ros2 run nav2_map_server --ros-args -p yaml_filename:=/home/sierra-95/Documents/robodojo/slam/michael.yaml
+ros2 lifecycle nodes
+ros2 lifecycle get /map_server
+ros2 lifecycle set /map_server 1 # configure
+ros2 lifecycle set /map_server 3 # activate
